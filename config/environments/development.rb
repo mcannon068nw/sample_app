64f0b37e5c031ad2d21fcb2a9394d6c73aa40ca9
Matthew Cannon <Matthew.Cannon2@nationwidechildrens.org>
2021-07-19 14:30:30 +0000

    Hello world

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,12 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts.clear
+  
+  
 end
+
+class Application < Rails::Application
+  config.web_console.permissions = '69.24.144.19'
+end
+
